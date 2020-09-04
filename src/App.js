01263595354e8054d19cb1500dc38de1b5e2bcff
brandonyrigan <brandonayrigan@gmail.com>
2020-09-04 10:03:21 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -7,16 +7,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>
+          Tinderfy
+        </h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://spotify.com"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Spotify
         </a>
       </header>
     </div>
